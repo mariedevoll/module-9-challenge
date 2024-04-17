7ea9620bf1ce1eb5fe991e1b3dd1b81cffd491c3
mariedevoll <marie.devoll@gmail.com>
2024-04-16 20:03:29 -0400

    created my function for writing the readme file

--- index.js
+++ index.js
@@ -44,11 +44,16 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    return fs.writeToFile
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((inquirerResponses) => {
+        console.log("Generating README...");
+        writeToFile("README.md", generateMarkdown({...inquirerResponses}))
+    })
+}
 
 // Function call to initialize app
 init();
